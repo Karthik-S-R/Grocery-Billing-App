711774247c20098aa3d7ab5c1352e67ca46d4b82
Karthik-S-R <karthiksr008@gmail.com>
2024-09-11 23:12:14 +0530

    feat: added Bill view component, tailwind css and angular material

---
+++ src/app/bill-view/bill-view.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-bill-view',
+  standalone: true,
+  imports: [],
+  templateUrl: './bill-view.component.html',
+  styleUrl: './bill-view.component.scss'
+})
+export class BillViewComponent {
+
+}
